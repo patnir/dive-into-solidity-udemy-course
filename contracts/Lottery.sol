73e04717e480fb97dba3d8d87afae8a8308cd850
Rahul Patni <rahul20patni@gmail.com>
2022-07-31 20:28:41 -0700

    finished exercise

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,36 +11,52 @@ contract Lottery {
 
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
         // TODO: require each player to send exactly 0.1 ETH
         // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether, "Send exactly 1 ether");
+        players.push(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
     }
 
     // returning the contract's balance in wei
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view onlyOwner returns (uint256) {
         // TODO: restrict this function so only the owner is allowed to call it
         // TODO: return the balance of this address
+        return address(this).balance;
     }
 
     // selecting the winner
-    function pickWinner() public {
-        // TODO: only the owner can pick a winner 
+    function pickWinner() public onlyOwner returns (bool, bytes memory) {
+        // TODO: only the owner can pick a winner
         // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+        require(players.length > 2, "NOT_ENOUGH_PLAYERS");
 
         uint256 r = random();
         address winner;
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
+        // TODO: compute an unsafe random index of the array and assign it to the winner variable
+        winner = players[r % players.length];
 
         // TODO: append the winner to the gameWinners array
+        gameWinners.push(winner);
 
         // TODO: reset the lottery for the next round
+        delete players;
 
         // TODO: transfer the entire contract's balance to the winner
+        (bool success, bytes memory returnBytes) = winner.call{
+            value: address(this).balance
+        }("");
+        return (success, returnBytes);
     }
 
     // helper function that returns a big random integer
